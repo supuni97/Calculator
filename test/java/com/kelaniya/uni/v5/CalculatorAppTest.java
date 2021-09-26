392a6cad7fe4a78438747b5121d189e9ab5a36bc
Supuni Sithara <73241855+supuni97@users.noreply.github.com>
2021-09-26 12:02:31 +0530

    Add unit testing 2

---
+++ test/java/com/kelaniya/uni/v5/CalculatorAppTest.java
@@ -0,0 +1,9 @@
+package com.kelaniya.uni.v5;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CalculatorAppTest{
+
+}
